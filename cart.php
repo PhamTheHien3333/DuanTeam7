0b069ea3a2bd3b6c49476fbe58e0be20e1546707
hoangDe11111 <147363399+hoangDe11111@users.noreply.github.com>
2023-12-03 00:36:43 +0700

    giohang
    giohang

---
+++ cart.php
@@ -0,0 +1,136 @@
+<?php
+if (empty($_COOKIE['cart'])) {
+    header('location: shop.php');
+}
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="img/favicon.ico" rel="icon">
+
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body class="main-layout inner_page">
+    <!-- loader  -->
+    <!-- end loader -->
+    <!-- header -->
+    <?php include './header-cart.php' ?>
+    <!-- end header -->
+    <!-- contact -->
+    <div class="text-center">
+        <h1 class="font-weight-semi-bold my-5">Gio hang</h1>
+    </div>
+    <div class="contact">
+        <div class="container">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Hình ảnh</th>
+                        <th>Tên</th>
+                        <th>Giá</th>
+                        <th>Số lượng</th>
+                        <th>Tổng tiền</th>
+                        <th width="15%"></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    // require '../connect.php';
+                    $cookie_data = $_COOKIE['cart'];
+                    $cart_data = json_decode($cookie_data, true);
+
+                    $tongtatca = 0;
+                    // var_dump($cart_data);
+                    foreach ($cart_data as $sp) :
+                        $tongtien = $sp['sale'] * $sp['soluong'];
+                        $tongtatca += $tongtien;
+                    ?>
+                        <tr>
+                            <td scope="row"><?= $sp['productId'] ?></td>
+                            <td><img class="card-img-top w-25 h-25" src="../uploads/products/<?= $sp['img'] ?>" alt="Card image cap"></td>
+                            <td><?= $sp['name'] ?></td>
+                            <td class="pr-3"> <?= number_format($sp['sale']) ?>đ</td>
+
+                            <td width="200px">
+                                <!-- cập nhật số lượng sản phẩm  -->
+
+                                <form action="cart-xuly.php" method="post">
+                                    <input type="number" name="soluong" value="<?= $sp['soluong'] ?>" onchange="this.form.submit()" class="form-control" min=1>
+                                    <input type="hidden" name="productId" value="<?= $sp['productId'] ?>">
+                                    <input type="hidden" name="name" value="<?= $sp['name'] ?>">
+                                    <input type="hidden" name="sale" value="<?= $sp['sale'] ?>">
+                                    <input type="hidden" name="img" value="<?= $sp['img'] ?>">
+                                    <input type="hidden" name="sua">
+
+                                </form>
+
+                            </td>
+                            <td> <?= number_format($tongtien) ?>đ</td>
+                            <td class="text-right">
+                                <!-- xóa sản phẩm trong giỏ hàng  -->
+                                <form action="cart-xuly.php" method="post">
+
+                                    <input type="hidden" name="productId" id="" value="<?= $sp['productId'] ?>">
+                                    <button class="btn btn-outline-danger" name="xoa">Xóa</button>
+                                </form>
+
+                            </td>
+                        </tr>
+                    <?php
+                    endforeach;
+                    ?>
+
+                    <tr>
+                        <td colspan="5">Tổng tiền tất cả</td>
+                        <td>
+                            <b><?= number_format($tongtatca) ?> đ</b>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5">
+                            <a href="checkout.php" class="btn btn-outline-cart">Thanh toán</a>
+                        </td>
+                        <td colspan="6" class="text-right">
+                            <!-- xóa giỏ hàng  -->
+                            <form action="cart-xuly.php" method="post">
+                                <button class="btn btn-outline-danger px-5" name="xoatatca">Xóa giỏ hàng</button>
+                            </form>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <?php include './footer.php' ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
